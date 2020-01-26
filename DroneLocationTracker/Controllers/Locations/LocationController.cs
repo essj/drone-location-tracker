69d81872db7bc9d740be4d03f6080d50a9e2cbf9
Jessica Xiao <jessicaoaix@gmail.com>
2020-01-27 02:43:20 +1300

    Add location controller. Add method to send locations

---
+++ DroneLocationTracker/Controllers/Locations/LocationController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using DroneLocationTracker.Data;
+using DroneLocationTracker.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace DroneLocationTracker.Controllers.Locations
+{
+	[Route("api/locations")]
+	[AllowAnonymous]
+	public class LocationController : Controller
+	{
+		private readonly Context _context;
+		private readonly IMapper _mapper;
+
+		public LocationController(Context context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+
+		private class LocationControllerProfile : Profile
+		{
+			public LocationControllerProfile()
+			{
+				CreateMap<LocationRequest, Location>(MemberList.Destination)
+					.ForMember(x => x.LocationId, o => o.Ignore())
+					.ForMember(x => x.Drone, o => o.Ignore())
+					.ForMember(x => x.Timestamp, o => o.MapFrom(x => DateTimeOffset.UtcNow));
+			}
+		}
+
+		/// <summary>
+		/// Send a location from a drone.
+		/// </summary>
+		[HttpPost("v1/send")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status403Forbidden)]
+		public async Task<IActionResult> SendLocation([FromBody] LocationRequest request)
+		{
+			var drone = await _context.Drones
+				.SingleOrDefaultAsync(x => x.DroneId == request.DroneId);
+
+			if (drone == null)
+				return Forbid();
+
+			var location = _mapper.Map<Location>(request);
+
+			_context.Add(location);
+
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+	}
+}
---
+++ DroneLocationTracker/Controllers/Locations/LocationRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DroneLocationTracker.Controllers.Locations
+{
+	public class LocationRequest
+	{
+		/// <summary>
+		/// Which drone this location is for.
+		/// </summary>
+		[Required]
+		public Guid DroneId { get; set; }
+
+		[Required]
+		public double Latitude { get; set; }
+
+		[Required]
+		public double Longitude { get; set; }
+	}
+}
