f7ea699db2a8e1dcb330f497eb7faf833d757ddf
Jessica Xiao <jessicaoaix@gmail.com>
2020-01-27 13:44:06 +1300

    Add Bootstrap

--- DroneLocationTracker/ClientApp/src/index.tsx
+++ DroneLocationTracker/ClientApp/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Router from './router';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 ReactDOM.render(
